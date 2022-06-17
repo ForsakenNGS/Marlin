e34d20686af7e9224c7ae945f772531cb9c18db6
Jens N <jens.n@godlike.biz>
2022-06-17 13:39:30 +0200

    User Configuration

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -464,7 +464,7 @@
 // hot end to reach the target temperature, but the time it takes to reach
 // the minimum temperature your thermistor can read. The lower the better/safer.
 // This shouldn't need to be more than 30 seconds (30000)
-//#define MILLISECONDS_PREHEAT_TIME 0
+#define MILLISECONDS_PREHEAT_TIME 10000
 
 // @section extruder
 
